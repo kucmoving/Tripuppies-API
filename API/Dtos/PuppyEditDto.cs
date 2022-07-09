2d13681a36980323184e040311a895dad0479ba9
kucmoving <kuc.moving@gmail.com>
2022-07-09 16:57:34 +0100

    fixed editbug

--- API/Dtos/PuppyEditDto.cs
+++ API/Dtos/PuppyEditDto.cs
@@ -7,5 +7,6 @@
         public string Story { get; set; }
         public string Region { get; set; }
         public string? Country { get; set; }
+        public int? Experience { get; set; }
     }
 }
