1d0397bbe448970a1649cd978c76d11837e9f3bb
kucmoving <kuc.moving@gmail.com>
2022-07-16 09:28:16 +0100

    fix commit

--- API/Dtos/PuppyEditDto.cs
+++ API/Dtos/PuppyEditDto.cs
@@ -6,13 +6,8 @@
         public string PreferStyle { get; set; }
         public string Story { get; set; }
         public string Region { get; set; }
-<<<<<<< HEAD
         public int? Experience { get; set; }
         public string NickName { get; set; }
 
-=======
-        public string? Country { get; set; }
-        public int? Experience { get; set; }
->>>>>>> e357a559256572903559e641d4e4a76e70d4275b
     }
 }
